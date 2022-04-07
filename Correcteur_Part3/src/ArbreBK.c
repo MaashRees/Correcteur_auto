26f772b5328802d375ab7d32509ea086e6b21914
MaashRees <seerharley03@gmail.com>
2022-04-08 00:25:43 +0200

    Ecriture de l'insertion et de la recherche. tests a realisés plus tard, fonction a réarrangées pour la comparaison de chaine de caractèere et de parametres adaptéset aussi verification si toutes les conditions sont vu et bien traités

--- Correcteur_Part3/src/ArbreBK.c
+++ Correcteur_Part3/src/ArbreBK.c
@@ -19,9 +19,97 @@ NoeudBK * alloue_ArbreBK(char * mot){
     return n;
 }
 
-int inserer_dans_ArbreBK(ArbreBK * A, char * mot);
+int inserer_dans_ArbreBK_aux(ArbreBK *A, char * mot, int * dist){
+    if(*A == NULL){
+        *A = alloue_ArbreBK(mot);
+        if(*A == NULL){
+            return 0;
+        } else{
+            *A->valeur = *dist;
+            return 1;
+        }
+    }
+    if(*A->valeur == *dist){
+        *dist = levenshtein(*A->mot, mot);
+        return inserer_dans_ArbreBK_aux(&(*A->filsG), mot, dist);
+    }
+    if(*A->valeur < *dist){
+        return inserer_dans_ArbreBK_aux(&(*A->frereD), mot, dist);
+    }
+    if(*A->valeur > *dist){
+        NoeudBK * new = alloue_ArbreBK(mot);
+        if(new != NULL){
+            new->frereD = *A->filsG;
+            *A->filsG = new;
+            new->valeur = *dist;
+            return 1;
+        }
+        return 0;
+    
+    }
+    return 0;
+}
+int inserer_dans_ArbreBK(ArbreBK * A, char * mot){
+    if(*A == NULL){
+        *A = alloue_ArbreBK(mot);
+        if(*A == NULL){
+            return 0;
+        } else{
+            *A->valeur = 0;
+            return 1;
+        }
+    }
+    int L;
+    L = levenshtein(*A->mot, mot);
+    if(*A->valeur == 0){
+        if(*A->filsG && *A->filsG->valeur > L){
+            NoeudBK * new = alloue_ArbreBK(mot);
+            if(new != NULL){
+                new->frereD = *A->filsG;
+                *A->filsG = new;
+                new->valeur = L;
+                return 1;
+            }
+            return 0;
+        }
+        return inserer_dans_ArbreBK_aux(&(*A->filsG), mot, &L);
+    }
+    return 0;
+}
 
-Liste rechercher_dans_ArbreBK(ArbreBK A, char * mot);
+Liste rechercher_dans_ArbreBK_aux(ArbreBK A, char * mot, int dist){
+    if(A == NULL){
+        return NULL;
+    }
+    if(A->mot == mot){
+        Liste new = alloue_liste(mot);
+        return new;
+    }
+    if(dist < A->valeur){
+        return NULL;
+    }
+    if(dist == A->valeur){
+        return rechercher_dans_ArbreBK_aux(A->filsG, mot, levenshtein(A->mot, mot));
+    }
+    if(dist > A->valeur){
+        return rechercher_dans_ArbreBK_aux(A->frereD, mot, dist);
+    }
+}
+
+Liste rechercher_dans_ArbreBK(ArbreBK A, char * mot){
+    if(A == NULL){
+        return NULL;
+    }
+    if(A->mot == mot){ /*strcmp a utilisé ceci ne compare que les premiers caracteres*/
+        Liste new = alloue_liste(mot);
+        return new;
+    }
+    int L = levenshtein(A->mot, mot);
+    if(A->valeur == 0 && A->filsG == NULL){
+        return NULL;
+    }
+    return rechercher_dans_ArbreBK_aux(A->filsG, mot, L);
+}
 
 ArbreBK creer_ArbreBK(File * dico);
 
